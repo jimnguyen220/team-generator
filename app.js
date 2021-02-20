8ef0ff5160ccaa7f564af7781cb76fd3ae60732d
Jim Nguyen <jimnguyen220@gmail.com>
2021-02-19 22:11:26 -0600

    Classes created and started coding questions

--- app.js
+++ app.js
@@ -9,10 +9,43 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { info } = require("console");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role on the team?",
+        choices: ["Manager", "Engineer", "Intern"]
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email address?",
+        
+
+    },
+        {
+        type: "input",
+        name: "id",
+        message: "What is the employee's id number?"
+
+    },
+    
+
+]).then((data) => {
+    fs.writeFile("./output/teamInfo.js", data, (err) =>
+    err ? console.error(err) : console.log("Employee added to roster"));
+});
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
